492c22cfd3e9d10bad4f47f4b3880edf57a8421f
Nick Hernandez <nicolas.hernandez144@Outlook.com>
2022-08-29 23:49:32 -0400

    added edit and delete buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,4 @@
 //This is for the pothead section
-window.addEventListener('load', () => {
-
   const form = document.querySelector("#new-name-form");
   const input = document.querySelector("#new-name-input");
   const list_el = document.querySelector("#names");
@@ -20,11 +18,50 @@ window.addEventListener('load', () => {
 
     const name_content_el = document.createElement("div");
     name_content_el.classList.add("content");
-    name_content_el.innerText = name;
 
     name_el.appendChild(name_content_el);
+
+    const name_input_el = document.createElement("input");
+    name_input_el.classList.add("name_text");
+    name_input_el.type =  "text";
+    name_input_el.value = name;
+    name_input_el.setAttribute("readonly", "readonly");
+
+    name_content_el.appendChild(name_input_el);
+
+    const name_actions_el = document.createElement("div");
+    name_actions_el.classList.add("actions");
+
+
+    const name_edit_el = document.createElement("button");
+    name_edit_el.classList.add("edit_button")
+    name_edit_el.innerHTML = "Edit"
+
+    const name_delete_el = document.createElement("button");
+    name_delete_el.classList.add("delete_button")
+    name_delete_el.innerHTML = "Delete"
+
+    name_actions_el.appendChild(name_edit_el);
+    name_actions_el.appendChild(name_delete_el);
+    
+    name_el.appendChild(name_actions_el)
+
     list_el.appendChild(name_el);
 
+    input.value = "";
 
+    name_edit_el.addEventListener('click', () => {
+      if (name_edit_el.innerText.toLowerCase() == "edit") {
+        name_input_el.removeAttribute("readonly");
+        name_input_el.focus();
+        name_edit_el.innerHTML = "Save";
+      } else {
+        name_input_el.setAttribute("readonly", "readonly"),
+        name_edit_el.innerText = "Edit";
+      }
+    })
+
+    name_delete_el.addEventListener('click', () => {
+      list_el.removeChild(name_el);
+    })
   })
-})
